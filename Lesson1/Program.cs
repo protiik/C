e2386b337aca3c11faac797510d9af1c55a68245
Vladimir <vladimir.protiik@yandex.ru>
2023-08-16 20:31:50 +1000

    Добавил папки первого и второго урока

---
+++ Lesson1/Program.cs
@@ -0,0 +1,81 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Задача 2
+// Console.WriteLine("Введите число 1 :");
+// string strNum1 = Console.ReadLine();
+// int num1 = int.Parse(strNum1);
+
+// Console.WriteLine("Введите число 2 :");
+// string strNum2 = Console.ReadLine();
+// int num2 = int.Parse(strNum2);
+
+// if (num1 > num2)
+// {
+//     Console.WriteLine($"Число {num1} больше {num2}");
+// }
+// else
+// {
+//     Console.WriteLine($"Число {num2} больше {num1}");
+// }
+
+
+//_______________________________________
+// Задача 4
+
+// Console.WriteLine("Введите число 1 :");
+// string strNum1 = Console.ReadLine();
+// int num1 = int.Parse(strNum1);
+
+// Console.WriteLine("Введите число 2 :");
+// string strNum2 = Console.ReadLine();
+// int num2 = int.Parse(strNum2);
+
+// Console.WriteLine("Введите число 3 :");
+// string strNum3 = Console.ReadLine();
+// int num3 = int.Parse(strNum3);
+
+// if (num1 > num2) {
+//     if (num1 > num3){
+//         Console.WriteLine($"Число наибольшее : {num1}");
+//     }else{
+//         Console.WriteLine($"Число наибольшее : {num3}");
+//     }
+// }else{
+//     if (num2 > num3){
+//           Console.WriteLine($"Число наибольшее : {num2}");
+//     }else{
+//         Console.WriteLine($"Число наибольшее : {num3}");
+//     }
+// }
+
+
+//_________________________________
+// Задача 6
+// Console.WriteLine("Введите число :");
+// string strNum1 = Console.ReadLine();
+// int num1 = int.Parse(strNum1);
+
+// if (num1 % 2 == 0) {
+//     Console.WriteLine("Число чётное");
+// }
+// else{
+//     Console.WriteLine("Число не чётное");
+
+// }
+
+
+
+//_________________________
+// Задача 8
+
+// Console.WriteLine("Введите число :");
+// string strNum1 = Console.ReadLine();
+// int num1 = int.Parse(strNum1);
+
+// int i = 1;
+
+// while (i <= num1) {
+//     if (i % 2 == 0){
+//         Console.WriteLine(i);
+//     }
+//     i+=1;
+// }
