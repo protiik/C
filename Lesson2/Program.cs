288f3e17f38f9290f1c8995719d60e06ac6164b0
Vladimir <vladimir.protiik@yandex.ru>
2023-08-19 14:13:00 +1000

    Задача 15

--- Lesson2/Program.cs
+++ Lesson2/Program.cs
@@ -7,16 +7,54 @@
 // 918 -> 1
 
 
-Console.WriteLine("Введите число 1 :");
+// Console.WriteLine("Введите число :");
+// string strNum1 = Console.ReadLine();
+// int num1 = int.Parse(strNum1);
+
+
+// int result = num1 / 10;
+
+// Console.WriteLine(result);
+
+// int res2 = result%10;
+
+// Console.WriteLine(res2);
+
+
+// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет. Не использовать массив
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+// Console.WriteLine("Введите число 1 :");
+// string strNum1 = Console.ReadLine();
+// int num1 = int.Parse(strNum1);
+
+// int i = 0;
+
+// while i < 3
+// {
+//     if 
+//     i+=1
+// }
+
+
+
+
+// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.WriteLine("Введите число :");
 string strNum1 = Console.ReadLine();
 int num1 = int.Parse(strNum1);
 
-
-int result = num1 / 10;
-
-Console.WriteLine(result);
-
-int res2 = result%10;
-
-Console.WriteLine(res2);
+if ((num1 == 1) || (num1 == 2) || (num1 == 3)  || (num1 == 4) || (num1 == 5)) {
+    Console.WriteLine("Нет");
+}else if ((num1 == 6) || (num1 == 7)) {
+    Console.WriteLine("Да");
+}else{
+    Console.WriteLine("Число не подходит под день недели");
+}
 
