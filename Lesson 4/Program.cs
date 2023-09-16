e29e56d6ad83d1fed9efb2f2ce8444c948952cdf
Vladimir <vladimir.protiik@yandex.ru>
2023-09-16 21:39:03 +1000

    Выполнено задание 4

--- Lesson 4/Program.cs
+++ Lesson 4/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+int FillNumber(string message){
+    Console.WriteLine($"Введите число {message} :");
+    int num = int.Parse(Console.ReadLine());
+    return num;
+}
+int GetPow(int num1, int num2){
+    int pow = num1;
+    for (int i = 1; i < num2; i++){
+        pow*=num1;
+    }
+    return pow;
+}
+
+
+int numA = FillNumber("A");
+int numB = FillNumber("B");
+
+Console.WriteLine($"Ответ : {GetPow(numA, numB)}");
