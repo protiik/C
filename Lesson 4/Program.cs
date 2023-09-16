44c6ee88d3130b6d8ee9f7f1b9a13e8cd3d31863
Vladimir <vladimir.protiik@yandex.ru>
2023-09-16 22:21:13 +1000

    Задача 27

--- Lesson 4/Program.cs
+++ Lesson 4/Program.cs
@@ -2,21 +2,46 @@
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
 
-int FillNumber(string message){
-    Console.WriteLine($"Введите число {message} :");
-    int num = int.Parse(Console.ReadLine());
-    return num;
-}
-int GetPow(int num1, int num2){
-    int pow = num1;
-    for (int i = 1; i < num2; i++){
-        pow*=num1;
-    }
-    return pow;
-}
+// int FillNumber(string message){
+//     Console.WriteLine($"Введите число {message} :");
+//     int num = int.Parse(Console.ReadLine());
+//     return num;
+// }
+// int GetPow(int num1, int num2){
+//     int pow = num1;
+//     for (int i = 1; i < num2; i++){
+//         pow*=num1;
+//     }
+//     return pow;
+// }
 
 
-int numA = FillNumber("A");
-int numB = FillNumber("B");
+// int numA = FillNumber("A");
+// int numB = FillNumber("B");
 
-Console.WriteLine($"Ответ : {GetPow(numA, numB)}");
+// Console.WriteLine($"Ответ : {GetPow(numA, numB)}");
+
+// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+// int FillNumber(string message){
+//     Console.WriteLine($"Введите число {message} :");
+//     int num = int.Parse(Console.ReadLine());
+//     return num;
+// }
+
+// int GetSumNumbers(int num){
+//     int  result = 0;
+//     for (int i = 0; i<=num ; i++){
+//         int ostat = num % 10;
+//         Console.WriteLine(ostat);
+//         num = num/10;
+//         result += ostat;
+//     }
+//     return result;
+// }
+// int number = FillNumber(" ");
+
+// Console.WriteLine($"Ответ : {GetSumNumbers(number)}");
